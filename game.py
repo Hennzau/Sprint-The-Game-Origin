2f858603f472ba671450b90965db3b2c40a1a845
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-15 13:27:13 +0100

    game update

--- game.py
+++ game.py
@@ -1,12 +1,19 @@
-#class which updates the game (logic and render) at each passage through the main loop
+#importations
+from level.level import Level
+from level.grid import Grid
+from level.obstacle import colors
+from level.player import Player
+
+#class game which updates the game (logic and render) at each passage through the main loop
 
 class Game :
     def __init__(self):
         self.levels = []
-        #afficher le début du jeu 
+        self.levels.append(Level((20,20),[(0,0)],[colors["red"]]))
+        
     def update(self,delta_time):
-        #update le player et la grille
-        pass
+        player.update()
+
     def render (self):
         #update le rendu
         pass
