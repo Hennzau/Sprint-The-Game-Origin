3d73589e9dfd1e87e2219d2999c9664991f10b6a
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-15 11:27:05 +0100

    update

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 ### Main loop of the game###
 
 ## Imports ##
-import time
 import pygame
+from game import Game
+from render.window import Window
+from level.obstacle import colors
 ## ----------------- ##
 
 
 def main():
-    start = time.time()
-    end = time.time()
-    deltatime = end-start
+    surface = Window(800, 800, "test")
+    game = Game()
+    while True:
+        start = pygame.time.get_ticks()
+        surface.clear(colors["darkgrey"])
+        end = pygame.time.get_ticks()
+        deltatime = (end-start)/1000
+        game.update(deltatime)
+        surface.flip()
 
 
 if __name__ == '__main__':
