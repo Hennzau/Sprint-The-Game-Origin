e3766bf030e53a894390a2f20db725a9acfed703
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-15 10:56:17 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,16 @@
+### Main loop of the game###
+
+## Imports ##
+import time
+import pygame
+## ----------------- ##
+
+
+def main():
+    start = time.time()
+    end = time.time()
+    deltatime = end-start
+
+
+if __name__ == '__main__':
+    main()
