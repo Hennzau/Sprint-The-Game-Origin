b16bcd4f46f78e46fc5e4308adde7a738c81a109
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-15 10:28:04 +0100

    created Player class

---
+++ level/player.py
@@ -0,0 +1,16 @@
+### Managment of the player object ###
+
+## Import ##
+import numpy as np
+## ---------------- ##
+
+
+class Player:
+    # Player object has a color, a position and a velocity
+    def __init__(self, color, x_init, y_init, x_velocity, y_velocity):
+        self.color = color
+        self.position = np.array([x_init, y_init])
+        self.velocity = np.array([x_velocity, y_velocity])
+
+    def update():
+        pass
