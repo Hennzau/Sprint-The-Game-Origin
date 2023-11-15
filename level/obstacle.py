7c336e6cfc686a5eb2ae918ef9bd1c16f422dd12
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-15 15:21:44 +0100

    nouvelle couleur darkblue

--- level/obstacle.py
+++ level/obstacle.py
@@ -1,6 +1,6 @@
 # dictionary of the used colors, in RGB (format understood by pygame)
 colors = {"red": (220, 20, 60), "blue": (106, 90, 205), "green": (154, 205, 50), "yellow": (255, 215, 0),
-          "darkgrey": (105, 105, 105)}
+          "darkgrey": (105, 105, 105), "darkblue":(25,25,112)}
 pixel_size = 50
 
 
