5a727a837da2ce68362554bbed8606389172751b
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-15 14:20:04 +0100

    update

--- game.py
+++ game.py
@@ -11,22 +11,25 @@ from level.player import Player
 class Game:
     def __init__(self):
         self.levels = []
+        self.cursor = None
         self.levels.append(Level((20, 20), [(0, 0)], [colors["red"]]))
         self.is_open = True
 
     def update(self, delta_time):
+
         for event in events():
             if event.type == pygame.QUIT:
                 self.is_open = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    Player.update_velocity(-5, 0)
-                if event.key == pygame.K_RIGHT:
-                    Player.update_velocity(5, 0)
-                if event.key == pygame.K_UP:
-                    Player.update_velocity(0, 5)
-                if event.key == pygame.K_DOWN:
-                    Player.update_velocity(0, -5)
+                for player in (self.levels[(self.cursor)]).players:
+                    if event.key == pygame.K_LEFT:
+                        player.update_velocity(-5, 0)
+                    if event.key == pygame.K_RIGHT:
+                        player.update_velocity(5, 0)
+                    if event.key == pygame.K_UP:
+                        player.update_velocity(0, 5)
+                    if event.key == pygame.K_DOWN:
+                        player.update_velocity(0, -5)
 
     def render(self):
         # update le rendu
