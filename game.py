2bc4274690a6b2848c6e18c8c8760fdec4f61759
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-15 10:56:21 +0100

    Game

---
+++ game.py
@@ -0,0 +1,12 @@
+#class which updates the game (logic and render) at each passage through the main loop
+
+class Game :
+    def __init__(self):
+        self.levels = []
+        #afficher le début du jeu 
+    def update(self,delta_time):
+        #update le player et la grille
+        pass
+    def render (self):
+        #update le rendu
+        pass
