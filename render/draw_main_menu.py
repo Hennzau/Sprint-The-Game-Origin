aea5b577704248f73d75f76f653dffccde01f404
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-16 11:18:57 +0100

    update

--- render/draw_main_menu.py
+++ render/draw_main_menu.py
@@ -4,13 +4,13 @@ import pygame
 
 def draw_main_menu(surface):
     pygame.font.init()
-    height,width = (1280,720)
+    width,height = (1280,720)
     surface.surface.fill(colors["darkblue"])
-    font = pygame.font.Font("GamepauseddemoRegular-RpmY6.otf", 100)
-    title = font.render('SPRINT - THE GAME', True, colors["white_yellow"])
-    start_button = font.render('Start', True, colors["white_yellow"])
-    surface.blit(title, (width/2 - title.get_width()/2, height/2 - title.get_height()/2))
-    surface.blit(start_button, (width/2 - start_button.get_width()/2, height/2 + start_button.get_height()/2))
+    font = pygame.font.Font("GamepauseddemoRegular-RpmY6.otf", 50)
+    title = font.render('SPRINT THE GAME', True, colors["white_yellow"])
+    start_button = font.render('Press Space to Start', True, colors["white_yellow"])
+    surface.blit(title, (width/2 - title.get_width()/2, height/2 - title.get_height()/2 - 100))
+    surface.blit(start_button, (width/2 - start_button.get_width()/2, height/2 + start_button.get_height()/2 - 100))
     pygame.display.update()
 
 
