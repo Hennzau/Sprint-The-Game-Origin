2eb18af3d1698eccf2757f0ffa2b806da8888328
Hennzau <dev@enzo-le-van.fr>
2023-11-16 18:29:42 +0100

    Player rendering

--- render/draw_player.py
+++ render/draw_player.py
@@ -1,5 +1,6 @@
 from level.player import Player
 import pygame
+
 from level.obstacle import pixel_size
 
 """"
@@ -8,4 +9,14 @@ draw_player is called upon to reprsent the player at all times for the viewer.
 
 
 def draw_player(player, surface):
-    pygame.draw.rect(surface, player.color, pygame.Rect(player.render_position[0], player.render_position[1], pixel_size, pixel_size))
+    blend_surface = pygame.Surface((pixel_size, pixel_size))
+    pygame.draw.rect(blend_surface, (player.color[0] / 2, player.color[1] / 2, player.color[2] / 2),
+                     pygame.Rect(0, 0, pixel_size, pixel_size))
+
+    surface.blit(blend_surface,
+                 (player.render_position[0], player.render_position[1]),
+                 special_flags=pygame.BLEND_RGB_ADD)
+
+    pygame.draw.rect(surface, player.color,
+                     pygame.Rect(player.render_position[0] + pixel_size / 10,
+                                 player.render_position[1] + pixel_size / 10, pixel_size * 8 / 10, pixel_size * 8 / 10))
