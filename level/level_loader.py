b0dbac09426f6d36272a59c65955f29ab0d99de4
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-16 09:28:43 +0100

    level loader from json and level1 in json

--- level/level_loader.py
+++ level/level_loader.py
@@ -1,14 +1,37 @@
+###Imports###
 import json
-"""
 
 from level.level import Level
 from level.obstacle import Obstacle, colors
 
-"""
+### build level function
+''' 
+this function takes a level written in a json file
+it returns the level objet that can be read by our python code
+'''
 
 def build_level (level_json): #level_json format : 'data.json'
     json_file = open(level_json)
     data = json.load(json_file) #data is a json object, like a dictionary
 
+    #recuperation of the data
+    size = data["size"]
+    initial_positions_table = data["initial_positions"]
+    final_positions_table = data ["final_positions"]
+    colors = data ["colors"]
+
+    #creation of the lists of tuples for initial and final positions
+    initial_positions_tuple =[(initial_positions_table[i][0],initial_positions_table[i][1])for i in range len(initial_positions_table)]
+    final_positions_tuple = [(final_positions_table[i][0],final_positions_table[i][1])for i in range len(final_positions_table)]
+    #creation of the level
+    level = Level(size,initial_positions_tuple , colors, final_positions_tuple)
+
+    #creation of the obstacles
     for obstacle in data["obstacles"]:
-        print(obstacle)
+        if len(obstacle)==3:
+            Level.grid.obstacles[obstacle[0],obstacle[1]]=Obstacle(obstacle[2])
+        if len(obstacle)==4 :
+            Level.grid.obstacle[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3])
+        if len(obstacle)==5:
+            Level.grid.obstacle[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3],obstacle[4])
+    return level
