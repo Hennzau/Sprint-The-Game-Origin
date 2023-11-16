7c8c6e47ec64246adc30a54f03ecd1f6303e7d26
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-16 09:50:00 +0100

    level loader update

--- level/level_loader.py
+++ level/level_loader.py
@@ -18,20 +18,22 @@ def build_level (level_json): #level_json format : 'data.json'
     size = data["size"]
     initial_positions_table = data["initial_positions"]
     final_positions_table = data ["final_positions"]
-    colors = data ["colors"]
+    initial_colors = data ["colors"]
 
     #creation of the lists of tuples for initial and final positions
-    initial_positions_tuple =[(initial_positions_table[i][0],initial_positions_table[i][1])for i in range len(initial_positions_table)]
-    final_positions_tuple = [(final_positions_table[i][0],final_positions_table[i][1])for i in range len(final_positions_table)]
+    initial_positions_tuple =[(initial_positions_table[i][0],initial_positions_table[i][1]) for i in range (len(initial_positions_table))]
+    final_positions_tuple = [(final_positions_table[i][0],final_positions_table[i][1])for i in range (len(final_positions_table))]
+    #creation of the list of colors in rgb format
+    initial_colors_rgb = [colors[color_name]for color_name in initial_colors]
     #creation of the level
-    level = Level(size,initial_positions_tuple , colors, final_positions_tuple)
+    level = Level(size,initial_positions_tuple , initial_colors_rgb, final_positions_tuple)
 
     #creation of the obstacles
     for obstacle in data["obstacles"]:
         if len(obstacle)==3:
-            Level.grid.obstacles[obstacle[0],obstacle[1]]=Obstacle(obstacle[2])
+            level.grid.obstacles[obstacle[0],obstacle[1]]=Obstacle(obstacle[2])
         if len(obstacle)==4 :
-            Level.grid.obstacle[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3])
+            level.grid.obstacles[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3])
         if len(obstacle)==5:
-            Level.grid.obstacle[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3],obstacle[4])
+            level.grid.obstacles[obstacle[0],obstacle[1]]=Obstacle(obstacle[2],obstacle[3],obstacle[4])
     return level
