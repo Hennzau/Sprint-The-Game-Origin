e67b5b168fa4ad187ef6f9d0541e074b0f0cdb3b
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-20 12:24:23 +0100

    Fixxed sound

--- sound.py
+++ sound.py
@@ -4,40 +4,40 @@ import pygame
 def sound_collision():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Collision.wav')
-    my_sound.set_volume(0.1)
+    my_sound.set_volume(0.09)
     my_sound.play()
 
 
 def sound_victory():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Victory_new.wav')
-    my_sound.set_volume(0.15)
+    my_sound.set_volume(0.1)
     my_sound.play()
 
 
 def sound_swipe():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Swipe_new.wav')
-    my_sound.set_volume(0.03)
+    my_sound.set_volume(0.025)
     my_sound.play()
     pass
 
 
 def sound_background():
     pygame.mixer.init()
-    my_sound = pygame.mixer.Sound('assets/sounds/Soundtrack1.wav')
-    my_sound.set_volume(0.7)
+    my_sound = pygame.mixer.Sound('assets/sounds/Soundtrack-Sprint.mp3')
+    my_sound.set_volume(0.9)
     my_sound.play()
     pass
 
 def sound_button():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Boup.wav')
-    my_sound.set_volume(0.075)
+    my_sound.set_volume(0.135)
     my_sound.play()
 
 def sound_game_launched():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Game launched.wav')
-    my_sound.set_volume(0.15)
+    my_sound.set_volume(0.1)
     my_sound.play()
