49804828b9fc8ba7f6a126a58e0b6cc57f3693d8
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-20 09:58:14 +0100

    level editor object

---
+++ editor/draw_level_editor.py
@@ -0,0 +1,5 @@
+from editor.level_editor import LevelEditor
+
+
+def draw_level_editor (level_editor, surface):
+    level_editor.level_render.render(surface.py_surface)
---
+++ editor/level_editor.py
@@ -0,0 +1,30 @@
+## Imports
+from level.grid import Grid
+from level.level import Level
+from level.obstacle import Obstacle, colors
+from render.level_render import LevelRender
+
+
+class LevelEditor :
+
+    def __init__(self, size):
+        self.size = size
+        self.level = Level(size, [], [], [])
+        self.players = []
+        self.colors = ["red", "blue", "green", "yellow", "darkgrey"]
+        self.color_cursor = None
+        self.color_switcher = False
+        self.level_render=LevelRender(self.level)
+        
+        
+    def new_player (self, color, initial_position, final_position ):
+        self.level.initial_colors.append(color)
+        self.level.initial_positions.append(initial_position)
+        self.level.final_positions.append(final_position)
+        self.level.grid.obstacle[initial_position[0],initial_position[1]]=Obstacle(colors[color], start=True)
+        self.level.grid.obstacle[final_position[0],final_position[1]]=Obstacle(colors[color], end=True)
+        self.level_render=LevelRender(self.level)
+
+    def new_tile (self,position):
+        self.level.grid.obstacles[position[0], position[1]]=Obstacle (colors[self.colors[self.color_cursor]], self.color_switcher)
+        self.level_render=LevelRender(self.level)
