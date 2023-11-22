e5a9021400b88e0e7913c76703d326a7b6bfd587
Valot Juliette <juliette.valot@student-cs.fr>
2023-11-22 10:38:45 +0100

    Main menu commented

--- main_menu.py
+++ main_menu.py
@@ -4,16 +4,36 @@ from game import Game
 
 
 class MainMenu:
-
+    """
+    The Main Menu manages the selection of the levels and launches the game.
+    It is rended on the scren by the draw_main_menu object.
+    """
     def __init__(self):
+        """
+        The __init__ method initiliases the variables that will be used to manage the choosing of the level
+        """
         self.level_selected = None
         self.start_game = False
         self.sound = False
 
     def update_level(self, level):
+        """
+        This method is used to change the selected level
+
+        Parameters :
+        level (Level): the level you want to select
+        """
+
         self.level_selected = level
 
     def launch_game(self, game):
+        """
+        This method is used to change the state of the game to Launched, which launches the selected level
+
+        Parameters:
+        game (Game): the current game
+        """
+
         if self.start_game:
             game.cursor = self.level_selected
             game.stage = "Launched"
