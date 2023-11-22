37fe466a73dfc90486305c8503e14cb0bbabbd02
Delache Emmanuel <emmanuel.delache@student-cs.fr>
2023-11-22 16:09:23 +0100

    Added sound when colliding with color switcher

--- sound.py
+++ sound.py
@@ -7,21 +7,21 @@ import pygame
 def sound_collision():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Collision.wav')
-    my_sound.set_volume(0.065)
+    my_sound.set_volume(0.045)
     my_sound.play()
 
 
 def sound_victory():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Victory_new.wav')
-    my_sound.set_volume(0.075)
+    my_sound.set_volume(0.08)
     my_sound.play()
 
 
 def sound_swipe():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Swipe_new.wav')
-    my_sound.set_volume(0.025)
+    my_sound.set_volume(0.015)
     my_sound.play()
     pass
 
@@ -36,5 +36,11 @@ def sound_button():
 def sound_game_launched():
     pygame.mixer.init()
     my_sound = pygame.mixer.Sound('assets/sounds/Game launched.wav')
-    my_sound.set_volume(0.08)
+    my_sound.set_volume(0.075)
+    my_sound.play()
+
+def sound_color_switch():
+    pygame.mixer.init()
+    my_sound = pygame.mixer.Sound('assets/sounds/Color swticher.wav')
+    my_sound.set_volume(0.025)
     my_sound.play()
